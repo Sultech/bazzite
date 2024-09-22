3c9cfb06319ad145d9e78ea6add6d5eb599671ea
Sul <30802163+Sultech@users.noreply.github.com>
2024-09-22 02:50:33 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+rpm-ostree remove wireplumber
+
 # add repo 
 curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
 
