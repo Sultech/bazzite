93efbb9a0df086c1628ad6f676c4c51ba4d7867a
Sul <30802163+Sultech@users.noreply.github.com>
2024-09-22 02:57:32 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,13 +12,12 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf remove wireplumber
 
 # add repo 
 curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 # this installs a package from fedora repos
-rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin pipewire-media-session
+rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
