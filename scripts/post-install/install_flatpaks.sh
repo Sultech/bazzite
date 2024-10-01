c6b76e738f15a406712243e8527da5d54a874ff2
Sul <30802163+Sultech@users.noreply.github.com>
2024-10-01 01:24:09 +0100

    Update install_flatpaks.sh

--- scripts/post-install/install_flatpaks.sh
+++ scripts/post-install/install_flatpaks.sh
@@ -21,7 +21,6 @@ python3 -m venv maestral-venv
 source maestral-venv/bin/activate
 python3 -m pip install --upgrade maestral
 python3 -m pip install --upgrade 'maestral[gui]'
-sudo cp -R /usr/share/gnome-shell/extensions /home/$USER/.local/share/gnome-shell/
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
 
