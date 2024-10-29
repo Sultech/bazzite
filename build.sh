300584fc3f2ce1bfd432bbf5ca1551d74b1962c6
Sul <30802163+Sultech@users.noreply.github.com>
2024-10-29 15:18:54 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ dnf remove wireplumber -y
 
 # add docker repo, installing packages, adding service to fix docker in distrobox 
 curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
-rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin pipewire-media-session dotnet-sdk-8.0 aspnetcore-runtime-8.0 dotnet-runtime-8.0 gcc #yaru-theme gnome-shell-extension-pop-shell 
+rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin pipewire-media-session dotnet-sdk-8.0 aspnetcore-runtime-8.0 dotnet-runtime-8.0 gcc selinux-policy-targeted #yaru-theme gnome-shell-extension-pop-shell 
 systemctl enable docker
 mkdir /etc/scripts/
 curl -Lo /etc/scripts/docker-fix.sh https://raw.githubusercontent.com/Sultech/bazzite/refs/heads/main/scripts/docker-fix.sh
